ec8f000ccde6d0af9cb136f23e9b1c1ad29b039e
emilainezx <emilaine125b@gmail.com>
2026-05-20 21:43:39 -0300

    feat: add funcionarios CRUD

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
+const FuncionarioController = require("./controllers/FuncionarioController");
 
 const routes = Router();
 
 routes.get("/health", (req, res) => {
   return res.status(200).json({ message: "Server on" });
 });
+
+routes.post('/funcionarios', FuncionarioController.criar);
+routes.get('/funcionarios', FuncionarioController.listar);
+routes.get('/funcionarios/:id', FuncionarioController.buscarPorId);
+routes.put('/funcionarios/:id', FuncionarioController.atualizar);
+routes.delete('/funcionarios/:id', FuncionarioController.deletar);
 
 module.exports = routes;
